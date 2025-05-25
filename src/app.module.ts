ff761b45acbe574f0c12fb102621b453926405dc
Aditiarzky <aditiarizkyutama@gmail.com>
2025-05-25 07:10:38 +0700

    [fix] remove rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './modules/auth/auth.module';
-import { UsersModule } from './modules/users/users.module';
-import { PrismaService } from 'prisma/prisma.service';
-import { RateLimiterModule, RateLimiterGuard } from 'nestjs-rate-limiter';
-import { APP_GUARD } from '@nestjs/core';
 import { GenresModule } from './modules/genres/genres.module';
 import { BooksModule } from './modules/books/books.module';
 import { ChaptersModule } from './modules/chapters/chapters.module';
+import { PrismaService } from '../prisma/prisma.service';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [
-    GenresModule, 
-    BooksModule, 
-    ChaptersModule,
-    AuthModule,
-    UsersModule,
-    RateLimiterModule.register({
-      points: 10, 
-      duration: 60, 
-      errorMessage: 'Too many requests, please try again later',
-    }),
-  ],
-  providers: [
-    PrismaService,
-    {
-      provide: APP_GUARD,
-      useClass: RateLimiterGuard,
-    },
-  ],
+  imports: [GenresModule, BooksModule, ChaptersModule, UsersModule, AuthModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
